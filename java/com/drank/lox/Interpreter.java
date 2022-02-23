c9a4b88d1e66157fabfa81eef7ef145df863c763
Daniel Rank <dwrank@gmail.com>
2022-02-23 01:21:32 -0800

    Interpreter: Add - Expression evaluator

---
+++ java/com/drank/lox/Interpreter.java
@@ -0,0 +1,89 @@
+package com.drank.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESSER:
+                return (double)left < (double)right;
+            case LESSER_EQUAL:
+                return (double)left <= (double)right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Object visitConditionalExpr(Expr.Conditional expr) {
+        Object condition = evaluate(expr.condition);
+        if (isTruthy(condition)) {
+            return  evaluate(expr.thenBranch);
+        }
+        return evaluate(expr.elseBranch);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) { return false; }
+        if (object instanceof Boolean) { return (boolean)object; }
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) { return true; }
+        if (a == null) { return false; }
+        return a.equals(b);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);  // => visit<Expr Type>(expr)
+    }
+}
